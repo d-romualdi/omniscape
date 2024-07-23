ab96dd6f25f1a229cee97ca8893e6d194dbcb239
Carina-Firkowski <carina.firkowski@apexrms.com>
2024-07-23 17:14:52 -0400

    Delete templates folder

--- src/templates/omniscapeExample.ssim.data/Scenario-6/omniscape_Required/runOmniscape.jl
+++
@@ -1,5 +0,0 @@
-cd(raw"C:\gitprojects\omniscape\src\templates\omniscapeExample.ssim.data\Scenario-6\omniscape_Required")
-
-using Pkg; Pkg.add(name="Omniscape", version="0.5.7")
-using Omniscape
-run_omniscape("config.ini")
